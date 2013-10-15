1cd1fa17aea5f37d01f9c3dd35db08da012164a4
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-10-15 11:18:41 +0200

    added abstract class attachment to the ForwardsGameState class

--- src/ForwardsGameState.java
+++ src/ForwardsGameState.java
@@ -1,29 +1,38 @@
 import java.util.Deque;
 import java.util.List;
+import java.util.Set;
 
 
-public class ForwardsGameState implements GameState {
+public class ForwardsGameState extends AbstractGameState {
 
-	@Override
-	public boolean isDone() {
+	ForwardsGameState (Board board, Player player, Set<Box> boxes) {
+		super(board, player, boxes);
+	}
+	
+	ForwardsGameState (Board board, Player player, Set<Box> boxes, Move lastMove) {
+		super(board, player, boxes, lastMove);
+	}
+
+	ForwardsGameState (Board board, Player player, Set<Box> boxes, Deque<Move> movesToHere) {
+		super(board, player, boxes, movesToHere);
+	}
+
+	@Override public boolean isDone() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public List<GameState> getNextBoxStates() {
+	@Override public List<GameState> getNextBoxStates() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public Deque<Move> getMovesToHere() {
+	@Override public Deque<Move> getMovesToHere() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
-	public int getDistanceToGoal() {
+	@Override public int getDistanceToGoal() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
