347e1357da383966e5df89ca168390a51de1282a
Fredrik Bystam <fredrik.bystam@gmail.com>
2013-10-16 14:51:14 +0200

    added some simple deadlock checking for forwards running

--- src/ForwardsGameState.java
+++ src/ForwardsGameState.java
@@ -64,11 +64,42 @@ public class ForwardsGameState extends AbstractGameState {
 			Set<Box> newBoxes = new HashSet<>(boxes);
 			newBoxes.remove(box);
 			newBoxes.add(movedBox);
-			moves.addLast (realMove); // TODO
-			GameState state = new ForwardsGameState(board, movedPlayer, newBoxes, moves);
-			nextStates.add (state);
+			moves.addLast (realMove);
+			ForwardsGameState state = new ForwardsGameState(board, movedPlayer, newBoxes, moves);
+			if (!isDeadlockState(state, movedBox))
+				nextStates.add (state);
 		}
 		return nextStates;
+	}
+	
+	private boolean isDeadlockState (ForwardsGameState state, Box movedBox) {
+		Set<Box> visitedBoxes = new HashSet<>();
+		return isDeadlockStateRecursive (state, movedBox, visitedBoxes);
+	}
+
+	private boolean isDeadlockStateRecursive(ForwardsGameState state, Box box,
+			Set<Box> visitedBoxes) {
+		visitedBoxes.add(box);
+		if (board.isGoal(box.getLocation()))
+			return false; // treat goals as non-deadlocks
+		if (!state.getPossibleMoves(box).isEmpty())
+			return false;
+		
+		List<Box> adjacentBoxes = getAdjacentBoxes(box, state.getBoxes(), visitedBoxes);
+		for (Box adjacentBox : adjacentBoxes)
+			if (!isDeadlockStateRecursive(state, adjacentBox, visitedBoxes))
+				return false;
+		return true;
+	}
+	
+	private List<Box> getAdjacentBoxes (Box box, Set<Box> newBoxes, Set<Box> visitedBoxes) {
+		List<Box> adjacentBoxes = new ArrayList<>();
+		for (Move move : Move.values()) {
+			Box adjacent = box.move(move);
+			if (newBoxes.contains(adjacent) && !visitedBoxes.contains(adjacent))
+				adjacentBoxes.add(adjacent);
+		}
+		return adjacentBoxes;
 	}
 
 	private List<Move> getPossibleMoves (Movable<?> m) {
